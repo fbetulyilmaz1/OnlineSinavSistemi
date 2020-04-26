4c4acab99c6f49fda43d085f05be901dc8c47797
fbetulyilmaz1 <63525312+fbetulyilmaz1@users.noreply.github.com>
2020-04-26 13:41:28 +0300

    Add files via upload

---
+++ Areas/Admin/Controllers/KursMerkeziController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineSinavSistemi.Bll.Abstract;
+using OnlineSinavSistemi.Model.Data;
+
+namespace OnlineSinavSistemi.UI.Areas.Admin.Controllers
+{
+    public class KursMerkeziController : AdminBaseController
+    {
+        IUnitOfWork service;
+        public KursMerkeziController(IUnitOfWork _service)
+        {
+            service = _service;
+        }
+
+        [HttpGet]
+        public IActionResult Listele()
+        {
+            ICollection<KursMerkezi> liste = service.KursMerkezi.GetAll(x => x.SilindiMi == false).Data;
+            return View(liste);
+        }
+
+
+        [HttpGet]
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(KursMerkezi model)
+        {
+            model.KayitTarihi = DateTime.Now;
+            model.SilindiMi = false;
+            service.KursMerkezi.Add(model);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Ekle));
+        }
+
+
+        [HttpPost]
+        public IActionResult Sil(int id)
+        {
+            service.KursMerkezi.SoftDelete(id);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Listele));
+        }
+
+
+        
+        public IActionResult Guncelle(int kursMerkeziId)
+        {
+            var sonuc = service.KursMerkezi.GetAll(x => x.Id == kursMerkeziId && x.SilindiMi == false).Data;
+            return View(sonuc);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(KursMerkezi model)
+        {
+            //ekleme işlemi ypaulacak.
+            model.GuncellemeTarihi = DateTime.Now;
+            service.KursMerkezi.Update(model);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Listele));
+        }
+
+        public IActionResult IlveIlceyeGoreSirala(string il,string ilce,string ad)
+        {
+            var liste = service.KursMerkezi.GetAll(x => x.Il == il && x.Ilce == ilce && x.Ad==ad).Data;
+            return View(liste);
+        }
+
+    }
+}
---
+++ Areas/Admin/Controllers/SoruController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using OnlineSinavSistemi.Bll.Abstract;
+using OnlineSinavSistemi.Model.Data;
+
+namespace OnlineSinavSistemi.UI.Areas.Admin.Controllers
+{
+    [EnableCors("AllowOrigin")]
+    public class SoruController : AdminBaseController
+    {
+        IUnitOfWork service;
+        public SoruController(IUnitOfWork _service)
+        {
+            service = _service;
+        }
+        public IActionResult Listele()
+        {
+            ICollection<Soru> liste = service.Soru.GetAll(x => x.SilindiMi == false).Data;
+            return View(liste);
+        }
+
+
+        [HttpGet]
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(Soru model)
+        {            
+            model.KayitTarihi = DateTime.Now;
+            model.SilindiMi = false;
+            service.Soru.Add(model);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Ekle));
+        }
+
+
+        [HttpPost]
+        public IActionResult Sil(int id)
+        {
+            service.Soru.SoftDelete(id);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Listele));
+        }
+
+
+
+        public IActionResult Guncelle(int SoruId)
+        {
+            var sonuc = service.Soru.GetAll(x => x.Id == SoruId && x.SilindiMi == false).Data;
+            return View(sonuc);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(Soru model)
+        {
+            //ekleme işlemi ypaulacak.
+            model.GuncellemeTarihi = DateTime.Now;
+            service.Soru.Update(model);
+            service.SaveChanges();
+            return RedirectToAction(nameof(Listele));
+        }
+
+        [HttpGet]
+        public IActionResult SoruGetByKursID(int bransId)
+        {
+            var sonuc = service.Soru.GetAll(x => x.BransId == bransId);
+            return View(sonuc);
+        }
+      
+    }
+}
