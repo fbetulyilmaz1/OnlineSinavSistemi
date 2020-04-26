69d7cf8a3f3f3314248d00c1e01cbe7555f0f8b8
fbetulyilmaz1 <63525312+fbetulyilmaz1@users.noreply.github.com>
2020-04-26 13:20:12 +0300

    Add files via upload

---
+++ OnlineSinavSistemi.Model/Data/Brans.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OnlineSinavSistemi.Model.Data
+{
+   public class Brans :Base
+    {
+        [Display(Name="Üst Branşı")]
+        public int? UstBransId { get; set; }
+        public string Ad { get; set; }
+        public string LogoUrl { get; set; }
+        
+        //navigation properties
+        [ForeignKey("UstBransId")]
+
+        public virtual Brans UstBransi { get; set; }
+    }
+}
+
